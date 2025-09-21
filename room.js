ad150a361e97050f19c5f1015f2c16624f54f47f
Proyecto00723 <gustavotellov27@gmail.com>
2025-09-21 14:31:38 -0600

    room.js

--- room.js
+++ room.js
@@ -1,9 +1,11 @@
-//Detectar si es creador desde la URL
+// Detectar si es creador desde la URL
 const urlParams = new URLSearchParams(window.location.search);
 const isCreator = urlParams.get("creator") === "true";
 
-
-// Ocultar controles si es espectador
-if (lisCreator) {
-  document.getElementById("video-controls").style.display = "none";
+// Ocultar controles si es espectador (Viewer)
+if (!isCreator) {
+  const controls = document.getElementById("video-controls");
+  if (controls) {
+    controls.style.display = "none";
+  }
 }
