47529e3054d465ae827732a43fa30a80b3529d7a
Xtremo Developer <gustavotellov27@gmail.com>
2026-02-05 18:38:23 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,17 +3,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebas
 
 // --- Firebase config (centralizado) ---
 const firebaseConfig = {
-  apiKey: "AIzaSyBQyCiEDDFE1qfCphr3-fE7MPwJ7nLoQss",
-  authDomain: "syncplay-c6b2a.firebaseapp.com",
-  databaseURL: "https://syncplay-c6b2a-default-rtdb.firebaseio.com",
-  projectId: "syncplay-c6b2a",
-  storageBucket: "syncplay-c6b2a.firebasestorage.app",
-  messagingSenderId: "484954878226",
-  appId: "1:484954878226:web:83e8ce3d502e0ab2d343cb"
+  apiKey: "AIzaSyBpUX-bHM7tO6azpEPj2-56k9QfZhkttkw",
+  authDomain: "stylishsteps-b67e8.firebaseapp.com",
+  projectId: "stylishsteps-b67e8",
+  storageBucket: "stylishsteps-b67e8.firebasestorage.app",
+  messagingSenderId: "855401239447",
+  appId: "1:855401239447:web:a0d9ca75fec39f3a21ead2",
 };
-
 // Inicializar Firebase
 export const app = initializeApp(firebaseConfig);
+
 
 
 
