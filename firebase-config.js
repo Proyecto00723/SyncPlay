28defdfe18af9fc9b7eb6cf525687f22460555f0
Xtremo Developer <gustavotellov27@gmail.com>
2026-02-05 18:41:37 -0500

    Update Firebase config with databaseURL and measurementId
    Added databaseURL and measurementId to Firebase config.

--- firebase-config.js
+++ firebase-config.js
@@ -5,13 +5,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebas
 const firebaseConfig = {
   apiKey: "AIzaSyBpUX-bHM7tO6azpEPj2-56k9QfZhkttkw",
   authDomain: "stylishsteps-b67e8.firebaseapp.com",
+  databaseURL: "https://stylishsteps-b67e8-default-rtdb.firebaseio.com",
   projectId: "stylishsteps-b67e8",
   storageBucket: "stylishsteps-b67e8.firebasestorage.app",
   messagingSenderId: "855401239447",
   appId: "1:855401239447:web:a0d9ca75fec39f3a21ead2",
+  measurementId: "G-HV24CLYXRF"
 };
 // Inicializar Firebase
 export const app = initializeApp(firebaseConfig);
+
 
 
 
